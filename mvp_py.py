47850c20209abdab2df1c12cbe9496d985d1c6d9
j-m-d-h@users.noreply.github.com <marcxdawson@gmail.com>
2020-01-09 12:48:34 -0500

    added new route images

--- mvp_py.py
+++ mvp_py.py
@@ -3,6 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from sklearn.neighbors import NearestNeighbors
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import base64
+import io
 
 
 
@@ -41,3 +44,26 @@ def favorites():
         track_list = track_list.append(process_input(i,False))
     track_list.drop_duplicates()
     return track_list.sample(30).to_json(orient="records")
+
+@app.route('/image/<song_id>', methods = ['GET'])
+def radar_map(song_id):
+    """Route for returning radar graph."""
+    c = ["acousticness", "danceability", "energy", "valence"] # Columns to Show
+    N = len(c)
+    values=df[df["track_id"] == song_id].iloc[0][c].tolist()
+    values += values[:1]
+    print(values)
+    angles = [n / float(N) * 2 * 3.141 for n in range(N)]
+    angles += angles[:1]
+    print(angles)
+    ax = plt.subplot(111, polar=True)
+    plt.xticks(angles[:-1], c, color='grey', size=8)
+    plt.yticks([], [], color="grey", size=7)
+    ax.set_rlabel_position(0)
+    ax.plot(angles, values, linewidth=1, linestyle='solid')
+    ax.fill(angles, values, 'b', alpha=0.1)
+    pic_bytes = io.BytesIO()
+    plt.savefig(pic_bytes, format="png")
+    pic_bytes.seek(0)
+    image_encoded = b"data:image/png;base64," + base64.b64encode(pic_bytes.read())
+    return image_encoded
